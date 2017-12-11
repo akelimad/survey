b88a3d8d3e43111d50cbed0c2485384c1751410f
M'hamed Chanchaf <m.chanchaf@gmail.com>
2017-12-11 09:48:47 +0000

    Créer un module candidat et ajouter la fonction pour tester si le candidat peut changer ces informations.

---
+++ modules/candidat/controllers/CandidatController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * CandidatController
+ *
+ * @author M'hamed Chanchaf <m.chanchaf@gmail.com>
+ *
+ * @package modules.candidat.controllers
+ * @version 1.0
+ * @since 1.5.0
+ */
+namespace Modules\Candidat\Controllers;
+
+class CandidatController
+{
+
+
+  
+  
+
+  
+} // END Class
---
+++ modules/candidat/models/Candidat.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Candidat
+ *
+ * @author M'hamed Chanchaf <m.chanchaf@gmail.com>
+ *
+ * @package modules.candidatures.controllers
+ * @version 1.0
+ * @since 1.5.0
+ */
+namespace Modules\Candidat\Models; 
+
+use App\Models\Model;
+use App\Session;
+
+class Candidat extends Model {
+
+	public static function canUpdateAccount($candidat=null)
+	{
+		if( is_null($candidat) ) {
+			$candidat = getDB()->findOne('candidats', 'candidats_id', Session::get('abb_id_candidat'));
+		} else {
+			$candidat = (object) $candidat; 
+		}
+		return (isset($candidat->can_update_account) && $candidat->can_update_account == 1);
+	}
+
+
+} // End model
