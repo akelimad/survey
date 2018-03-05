91fe5bb13b448e59d778b0ed7b654ddee4ad7033
M'hamed Chanchaf <m.chanchaf@gmail.com>
2018-03-05 14:17:17 +0000

    Installer la bibliotheque Faker pour remplir la base de donnée avec de faut contenu.

---
+++ src/Faker.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Faker
+ * 
+ * Generate fake data
+ *
+ * @author M'hamed Chanchaf <m.chanchaf@gmail.com>
+ *
+ * @package app
+ * @version 1.0
+ * @since 1.5.0
+ */
+namespace App;
+
+use Faker;
+
+class Faker
+{
+
+   
+} //End class
