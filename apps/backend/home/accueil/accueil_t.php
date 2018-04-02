fcb0ebe980e2ea5140b4f55abc63f777adbd8e5b
M'hamed Chanchaf <m.chanchaf@gmail.com>
2018-04-02 18:43:02 +0100

    Corriger le probblème de session déja commance dans la page d'accueil backend

--- apps/backend/home/accueil/accueil_t.php
+++ apps/backend/home/accueil/accueil_t.php
@@ -2,8 +2,9 @@
 
 
 
-session_start();
+// session_start();
 
+if (session_status() == PHP_SESSION_NONE) session_start();
 
 
 // if(isset($_SESSION['compte_v'])) {  header("Location: ../compte/");	 } 
